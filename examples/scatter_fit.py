acdf47a96124c1d10674b619b9b15918c3be8c66
lhk <lars.klein@rwth-aachen.de>
2018-10-14 00:01:27 +0200

    starting pattern for backprop

--- examples/scatter_fit.py
+++ examples/scatter_fit.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+# set up the data
 # circle one
 num_samples=100
 phi = np.linspace(0, np.pi * 5, num_samples)
@@ -27,4 +28,75 @@ labels = np.concatenate([labels1, labels2])
 
 plt.scatter(x, y, c=labels)
 plt.show()
+
+# set up the network
+from LinearNode import LinearNode
+from TanhNode import TanhNode
+from SigmoidNode import SigmoidNode
+
+np.random.seed(0)
+
+shapes = [10, 100, 1]
+input_size = 2
+nodes = []
+for shape in shapes:
+    W = np.random.rand(shape, input_size)
+    b = np.random.rand(input_size, 1)
+    input_size = shape
+
+    lin = LinearNode(W, b)
+    tanh = TanhNode()
+    nodes.append(lin)
+    nodes.append(tanh)
+
+sigm = SigmoidNode()
+nodes.append(sigm)
+
+def forward(x):
+    for node in nodes:
+        x= node.forward(x)
+    return x
+
+def backward(dy, ddy):
+    for node in reversed(nodes):
+        dy, ddy = node.backward(dy, ddy)
+    return dy, ddy
+
+def sample(batch_size):
+    indices = np.random.choice(200, batch_size)
+
+    batch_x = x[indices]
+    batch_y = y[indices]
+    batch = np.concatenate([batch_x, batch_y], axis=0)
+
+    targets = labels[indices]
+
+    return batch, targets
+
+def update(alpha=1e-1):
+    for node in nodes:
+        for param_id in node.param_ids:
+            # something like a relaxed newton step
+            node.params[param_id][:] = node.params[param_id] - alpha*node.d[param_id]/node.dd[param_id]
+
+
+# start the training process
+batch_size = 16
+while True:
+    # get a training sample
+    batch, targets = sample(batch_size=batch_size)
+
+    # forward pass
+    preds = forward(batch)
+
+    # compute and derive loss
+    loss = ((preds - targets)**2).mean()
+    dy = 2*(preds - targets)
+    ddy = 2
+
+    # backward pass
+    backward(dy, ddy)
+
+    # update params
+    update()
 
