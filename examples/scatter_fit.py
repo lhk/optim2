49fc21d099e0ca538f13250355a1029e3ff1eef7
lhk <lars.klein@rwth-aachen.de>
2018-10-13 23:50:28 +0200

    adding a spiral pattern

--- examples/scatter_fit.py
+++ examples/scatter_fit.py
@@ -1,15 +1,30 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-x = np.random.rand(2, 100)
-y = np.zeros((1,100))
-y[0, :50] = 1
-x[0,:50] = x[0,:50]-0.2
-x[0, 50:] = x[0,50:]+0.2
+# circle one
+num_samples=100
+phi = np.linspace(0, np.pi * 5, num_samples)
+r = np.linspace(0, 10, num_samples)
 
-x = x - x.min()
-x = x / x.max()
+x1 = np.cos(phi)*r
+y1 = np.sin(phi)*r
 
-plt.scatter(x=x[0,:], y=x[1,:], c=y[0,:])
+labels1 = np.zeros((num_samples,))
+
+# circle two
+num_samples=100
+phi = np.linspace(0 + np.pi/2, np.pi * 5 + np.pi/1.3, num_samples)
+r = np.linspace(0, 10, num_samples)
+
+x2 = np.cos(phi)*r
+y2 = np.sin(phi)*r
+
+labels2 = np.ones((num_samples,))
+
+x = np.concatenate([x1, x2])
+y = np.concatenate([y1, y2])
+labels = np.concatenate([labels1, labels2])
+
+plt.scatter(x, y, c=labels)
 plt.show()
 
