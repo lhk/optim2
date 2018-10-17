8439adba6717b21bb5980e4dad0fa01f62e02891
lhk <lars.klein@rwth-aachen.de>
2018-10-17 22:02:53 +0200

    adding tests for Sigmoid Node

---
+++ tests/LinearSigmoidNetworkTest.py
@@ -0,0 +1,31 @@
+import numpy as np
+import unittest
+
+from LinearNode import LinearNode
+from SigmoidNode import SigmoidNode
+from tests.NetworkTest import NetworkTest
+
+class LinearSquareNetworkTest(NetworkTest, unittest.TestCase):
+
+    def setUp(self):
+        super().setUp()
+
+        np.random.seed(0)
+
+        W1 = np.random.rand(4, 3)
+        b1 = np.random.rand(4, 1)
+        W2 = np.random.rand(1, 4)
+        b2 = np.random.rand(1, 1)
+
+        batch_size = 1
+
+        # first dimension is batch
+        self.x = np.random.rand(batch_size, 3, 1)
+
+        lin1 = LinearNode(W1, b1, batch_size)
+        act1 = SigmoidNode(4, batch_size)
+        lin2 = LinearNode(W2, b2, batch_size)
+        act2 = SigmoidNode(1, batch_size)
+
+        self.nodes=[lin1, act1, lin2, act2]
+
