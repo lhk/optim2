d88645c915c7c8fd3d96419481ee4472b09aad8d
lhk <lars.klein@rwth-aachen.de>
2018-10-17 22:01:01 +0200

    updating sigmoidNode to new notation

--- SigmoidNode.py
+++ SigmoidNode.py
@@ -1,47 +1,67 @@
 import numpy as np
 
-class SigmoidNode:
-    """
-    this is just a template
-    """
-    def __init__(self):
+class TanhNode:
+    def __init__(self, n, batch_size):
         #no params
         self.param_ids=[]
 
+        self.n = n
+        self.batch_size=batch_size
+
     def forward(self, x):
-        """
-        Computes the forward pass.
-        :param x:
-        :return:
-        """
+
         self.x= x
         self.exp_negx = np.exp(-x)
-        return 1/(1+self.exp_negx)
+        self.a = 1/(1+self.exp_negx)
+        return self.a
+
+    def backward_pass1(self, J_ya):
+        # J_ya is a row vector
+        assert J_ya.shape == (self.batch_size, 1, self.n)
+        self.J_ya = J_ya
+
+        self.update_J_ax()
+        self.J_yx = J_ya @ self.J_ax
+
+        return self.J_yx
 
 
-    def backward(self, dy, ddy):
-        """
-        Computes the backward pass from inflowing gradients.
-        Creating the first derivatives is a simple backward pass.
-        Calculating the second derivs is harder:
-        We already receive a second derivative, this has to be multiplied by the square of our first derivs.
-        Add to this our second deriv times the incoming first.
+    def update_J_ax(self):
+        n = self.n
+        batch_size = self.batch_size
+        self.J_ax = np.zeros((batch_size, n,n))
+        self.J_ax[:, np.arange(n), np.arange(n)] = (self.exp_negx/(self.exp_negx +1)**2).reshape((batch_size, n))
 
-        :param dy: first derivative
-        :param ddy: second derivative
-        :return:
-        """
-
-        # first derivative of output
-        dx = dy * self.exp_negx/(self.exp_negx +1)**2
-
-        # second derivative of output
-        dd_in = ddy * (self.exp_negx/(self.exp_negx +1)**2)**2
-        dd_own = dy * ((2*np.exp(-2*self.x)/(self.exp_negx+1)**3) - self.exp_negx/(self.exp_negx +1)**2)
-        ddx = dd_in + dd_own
-
-        return dx, ddx
+    def update_H_ax(self):
+        n = self.n
+        batch_size = self.batch_size
+        self.H_ax = np.zeros((batch_size, n, n, n))
+        self.H_ax[:, np.arange(n), np.arange(self.n), np.arange(self.n)] =  (((2*np.exp(-2*self.x)/(self.exp_negx+1)**3) - self.exp_negx/(self.exp_negx +1)**2)).reshape((batch_size, n))
 
 
+    def backward_pass2(self, H_ya):
 
+        m,n = self.n, self.n
+        batch_size = self.batch_size
 
+        # computing the first part of H_ax
+        # moving the indices h and k of H_ya to the first two axes
+        # creating two versions of J_ax that can be broadcast together
+        # after the broadcast, the indices of a will be the first two axes
+        # and the indices of x will be the last two
+        H_ya = H_ya.reshape((batch_size, m, m, 1, 1))
+        J_ax_1 = self.J_ax.reshape((batch_size, m, 1, n, 1))
+        J_ax_2 = self.J_ax.reshape((batch_size, 1, m, 1, n))
+
+        # summing over the first two axes (skipping batch_size)
+        I_yx = (H_ya * J_ax_1 * J_ax_2).sum(axis=1).sum(axis=1)
+
+        # computing the Hessian H_ax
+        self.update_H_ax()
+
+        # computing the second part of H_ax
+        J_ya_1 = self.J_ya.reshape((batch_size, m, 1, 1))
+        O_yx = (J_ya_1 * self.H_ax).sum(axis=1)
+
+        self.H_yx = I_yx + O_yx
+        return self.H_yx
