57739c2902ec32456e6809ecea359cb9d3995cb5
lhk <lars.klein@rwth-aachen.de>
2018-10-17 14:19:27 +0200

    wip: working on linearNode version

--- LinearNode.py
+++ LinearNode.py
@@ -24,16 +24,28 @@ class LinearNode():
         self.J_y["W"] = np.dot(J_ya.T, self.inp.T)
         self.J_y["b"] =J_ya # np.sum(J_ya.T, keepdims=True, axis=1)
 
-        self.J_ax = self.W.T
+        self.update_J_ax()
         self.J_yx = J_ya @ self.J_ax
 
         return self.J_yx
 
+    def update_J_ax(self):
+        self.J_ax = self.W.T
+
+    def update_H_ax(self):
+        # this is a linear mapping
+        # the second derivative is 0
+        self.H_ax = np.zeros((self.m, self.n, self.n))
+
     def backward_pass2(self, H_ya):
+
+        m,n = self.m, self.n
 
         # computing the first part of H_ax
         # moving the indices h and k of H_ya to the first two axes
-        # moving the indices j and j of I_yx to the last two axes
+        # creating two versions of J_ax that can be broadcast together
+        # after the broadcast, the indices of a will be the first two axes
+        # and the indices of x will be the last two
         H_ya = H_ya.reshape((m, m, 1, 1))
         J_ax_1 = self.J_ax.reshape((m, 1, n, 1))
         J_ax_2 = self.J_ax.reshape((1, m, 1, n))
@@ -42,7 +54,13 @@ class LinearNode():
         I_yx = (H_ya * J_ax_1 * J_ax_2).sum(axis=0).sum(axis=0)
 
         # computing the Hessian H_ax
-        # this is a 3d tensor, since our layer maps a vector to a vector
-        return self.ddy_ddin
+        self.update_H_ax()
+
+        # computing the second part of H_ax
+        J_ya_1 = self.J_ya.reshape((m, 1, 1))
+        O_yx = (J_ya_1 * self.H_ax).sum(axis=0)
+
+        self.H_yx = I_yx + O_yx
+        return self.H_yx
 
 
