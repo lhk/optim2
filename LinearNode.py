f1c02b1f5fa5f58868102ba18ec2ff841c73acc8
lhk <lars.klein@rwth-aachen.de>
2018-10-17 13:56:11 +0200

    wip: working on linearNode version

--- LinearNode.py
+++ LinearNode.py
@@ -5,6 +5,10 @@ class LinearNode():
         self.W = W
         self.b = b
 
+        # x has n entries, a has m entries
+        # m is the output dimension, n is the input dimension
+        self.m, self.n = self.W.shape
+
         self.J_y = {}
         self.H_y = {}
 
@@ -26,9 +30,19 @@ class LinearNode():
         return self.J_yx
 
     def backward_pass2(self, H_ya):
-        # this is a linear layer ddout_ddin = 0
-        self.ddy_ddin = (self.dout_din**2)@ddy_ddout
 
+        # computing the first part of H_ax
+        # moving the indices h and k of H_ya to the first two axes
+        # moving the indices j and j of I_yx to the last two axes
+        H_ya = H_ya.reshape((m, m, 1, 1))
+        J_ax_1 = self.J_ax.reshape((m, 1, n, 1))
+        J_ax_2 = self.J_ax.reshape((1, m, 1, n))
+
+        # summing over the first two axes
+        I_yx = (H_ya * J_ax_1 * J_ax_2).sum(axis=0).sum(axis=0)
+
+        # computing the Hessian H_ax
+        # this is a 3d tensor, since our layer maps a vector to a vector
         return self.ddy_ddin
 
 
